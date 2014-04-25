68d707adc1de5bfd9e2242d1640d8bf48e47df48
Borislav Angelov <borislav.angelov@gmail.com>
2014-04-25 22:26:50 +0300

    Fix version number

--- lib/StorageArea.php
+++ lib/StorageArea.php
@@ -29,7 +29,7 @@
  * @author    Bobby Angelov <bobby@servmask.com>
  * @copyright 2014 Yani Iliev, Bobby Angelov
  * @license   https://raw.github.com/borislav-angelov/storage-factory/master/LICENSE The MIT License (MIT)
- * @version   GIT: ..11.0.0
+ * @version   GIT: 1.0.0
  * @link      https://github.com/borislav-angelov/storage-factory/
  */
 
@@ -45,7 +45,7 @@ require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'StorageDirectory.php';
  * @author    Bobby Angelov <bobby@servmask.com>
  * @copyright 2014 Yani Iliev, Bobby Angelov
  * @license   https://raw.github.com/borislav-angelov/storage-factory/master/LICENSE The MIT License (MIT)
- * @version   GIT: ..11.0.0
+ * @version   GIT: 1.0.0
  * @link      https://github.com/borislav-angelov/storage-factory/
  */
 class StorageArea
